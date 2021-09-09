0a7f905e840668c34c235dc79692458554fffea8
Rinat Muhamedgaliev <rinatmuhamedgaliev@gmail.com>
2021-09-10 01:07:34 +0300

    Added SendAnimation

---
+++ src/main/java/dev/tobee/telegram/request/SendAnimation.java
@@ -0,0 +1,59 @@
+package dev.tobee.telegram.request;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import dev.tobee.telegram.model.Message;
+import dev.tobee.telegram.model.ResponseWrapper;
+import dev.tobee.telegram.request.body.SendAnimationBody;
+import dev.tobee.telegram.util.DefaultJsonMapper;
+import dev.tobee.telegram.util.DefaultObjectMapper;
+
+import java.net.URI;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Optional;
+
+public class SendAnimation implements Request<ResponseWrapper<Message>> {
+
+    private static final String METHOD = "sendAnimation";
+
+    private final DefaultObjectMapper mapper = new DefaultObjectMapper();
+    private final DefaultJsonMapper jsonMapper = new DefaultJsonMapper();
+
+    private static final TypeReference<ResponseWrapper<Message>> reference = new TypeReference<>() {
+    };
+
+    private final SendAnimationBody body;
+
+    public SendAnimation(SendAnimationBody body) {
+        this.body = body;
+    }
+
+    @Override
+    public String getMethod() {
+        return METHOD;
+    }
+
+    @Override
+    public TypeReference<ResponseWrapper<Message>> getResponseType() {
+        return reference;
+    }
+
+    @Override
+    public Optional<Map<Object, Object>> getBody() {
+        Map<Object, Object> bodyMap = mapper.convertToMap(body);
+
+        String photoFieldName = "animation";
+
+        if (bodyMap.containsKey(photoFieldName) && bodyMap.get(photoFieldName) instanceof String) {
+            bodyMap.put(photoFieldName, Paths.get(URI.create(String.valueOf(bodyMap.get(photoFieldName)))));
+        }
+
+        String replyMarkupFieldName = "reply_markup";
+
+        if (bodyMap.containsKey(replyMarkupFieldName)) {
+            bodyMap.put(replyMarkupFieldName, jsonMapper.convertToString(body.replyMarkup()));
+        }
+
+        return Optional.of(bodyMap);
+    }
+}
