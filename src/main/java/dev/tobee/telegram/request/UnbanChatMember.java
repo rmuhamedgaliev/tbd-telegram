1d222c9b9c85801ab92589587871310bb65e6b92
Rinat Muhamedgaliev <rinatmuhamedgaliev@gmail.com>
2021-10-02 00:06:56 +0300

    #31 fix

---
+++ src/main/java/dev/tobee/telegram/request/UnbanChatMember.java
@@ -0,0 +1,37 @@
+package dev.tobee.telegram.request;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import dev.tobee.telegram.model.ResponseWrapper;
+import dev.tobee.telegram.request.body.UnBanChatMemberBody;
+import dev.tobee.telegram.util.DefaultObjectMapper;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class UnbanChatMember implements Request<ResponseWrapper<Boolean>> {
+    private static final String METHOD = "unbanChatMember";
+    private static final TypeReference<ResponseWrapper<Boolean>> reference = new TypeReference<>() {
+    };
+    private final DefaultObjectMapper mapper = new DefaultObjectMapper();
+    private final UnBanChatMemberBody body;
+
+    public UnbanChatMember(UnBanChatMemberBody body) {
+        this.body = body;
+    }
+
+    @Override
+    public String getMethod() {
+        return METHOD;
+    }
+
+    @Override
+    public TypeReference<ResponseWrapper<Boolean>> getResponseType() {
+        return reference;
+    }
+
+    @Override
+    public Optional<Map<Object, Object>> getBody() {
+        Map<Object, Object> bodyMap = mapper.convertToMap(body);
+        return Optional.of(bodyMap);
+    }
+}
---
+++ src/main/java/dev/tobee/telegram/request/body/UnBanChatMemberBody.java
@@ -0,0 +1,12 @@
+package dev.tobee.telegram.request.body;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Optional;
+
+public record UnBanChatMemberBody(
+        @JsonProperty("chat_id") int chatId,
+        @JsonProperty("user_id") int userId,
+        @JsonProperty("only_if_banned") Optional<Boolean> onlyIfBanned
+) {
+}
