5186134038de11ea14b19377a72619d534ce16ad
Rinat Muhamedgaliev <rinatmuhamedgaliev@gmail.com>
2021-10-10 00:32:28 +0300

    Fixed #51 added GetChatMember

---
+++ src/main/java/dev/tobee/telegram/request/body/GetChatMemberBody.java
@@ -0,0 +1,9 @@
+package dev.tobee.telegram.request.body;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record GetChatMemberBody(
+        @JsonProperty("chat_id") long chatId,
+        @JsonProperty("user_id") long userId
+) {
+}
