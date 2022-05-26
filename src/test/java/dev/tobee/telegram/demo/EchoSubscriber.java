c1c164740fa8e619013300bd3abe6aff9de8d570
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2022-05-26 23:55:36 +0300

    Removed unused code

--- src/test/java/dev/tobee/telegram/demo/EchoSubscriber.java
+++ src/test/java/dev/tobee/telegram/demo/EchoSubscriber.java
@@ -2,24 +2,36 @@ package dev.tobee.telegram.demo;
 
 import dev.tobee.telegram.client.TelegramApiClient;
 import dev.tobee.telegram.model.message.Update;
-import dev.tobee.telegram.service.MessageService;
+import dev.tobee.telegram.request.body.SendMessageBody;
+import dev.tobee.telegram.request.message.SendMessage;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalLong;
 import java.util.concurrent.Flow;
 
 public class EchoSubscriber extends BaseSubscriber implements Flow.Subscriber<Update> {
 
-    private final MessageService messageService;
-
     protected EchoSubscriber(TelegramApiClient client) {
         super(client);
-        messageService = new MessageService(client);
     }
 
     @Override
     public void onNext(Update item) {
         item.message().flatMap(message -> message.text().filter(text -> text.startsWith("/echo"))).ifPresent(command -> {
                     var message = command.substring(command.indexOf(" ") + 1);
-                    messageService.sendReplyToMessage(item.message().orElseThrow().chat().id(),message, item.message().orElseThrow().messageId());
+                    client.postRequest(new SendMessage(new SendMessageBody(
+                            item.message().orElseThrow().chat().id(),
+                            message,
+                            Optional.empty(),
+                            List.of(),
+                            Optional.empty(),
+                            Optional.empty(),
+                            Optional.empty(),
+                            OptionalLong.empty(),
+                            Optional.empty(),
+                            Optional.empty()
+                    )));
                 }
         );
         super.onNext(item);
